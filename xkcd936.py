2f8b029e6cba8cae5bbdc790064897aa2701e367
Mark Egan-Fuller <mark.eganfuller@bytemark.co.uk>
2019-06-27 09:37:35 +0100

    Add #!.

--- xkcd936.py
+++ xkcd936.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import random
 import sys
 
@@ -6,7 +7,6 @@ with open('/usr/share/dict/words') as f:
     words = [l.strip() for l in f.readlines()]
 
 words = [w for w in words if "'" not in w]
-
 
 for i in range(0, int(sys.argv[1])):
     print(random.choice(words).capitalize(), end='')
